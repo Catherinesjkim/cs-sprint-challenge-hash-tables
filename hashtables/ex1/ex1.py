77bcb58d77229dc2b9b24eaca081bd895e0111d1
Catherine <catherinesjkim@gmail.com>
2020-09-13 20:10:52 -0700

    Add Updated ex1.py Because: - Updated my comments so that it is easier for others to read my code. - Added a driver code so that I can make sure that the code is working well in the ex1.py file.

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -23,19 +23,25 @@ def get_indices_of_item_weights(weights: list, length: int, limit: int) -> list:
         return None
     
     # Traverse arr only once. For each weight in arr, compute its complement limit - weight and check wether that complement is hashed so far
-    # If found the complement in the map, return a pair that consists of weight's and limit - weight's indices
-    # If not, hash the weight while using:
-    # weight = hash key
-    # array index = hash value
-    # Even if the same weight is found more than once, it doesn't matter because at the tiem of the lookup, we only need one item with that weight
     for i in range(length):
         weight = weights[i]
+        # If found the complement in the map, return a pair that consists of weight's and limit - weight's indices
         if weight in hashTable:
             value = hashTable[weight]
             return [i, value]
+        # If not, hash the weight while using:
+        # weight = hash key
+        # array index = hash value
+        # Even if the same weight is found more than once, it doesn't matter because at the time of the lookup, we only need one item with that weight
         diff = limit - weight
         hashTable[diff] = i
     # if such a pair doesn't exist, return an empty array
     return None
 
+# Driver code
+print(get_indices_of_item_weights([4, 6, 10, 15, 16], 5, 21))
 
+"""
+input: weights = [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
+output: [ 3, 1 ]  # since these are the indices of weights 15 and 6 whose sum equals 21
+"""
