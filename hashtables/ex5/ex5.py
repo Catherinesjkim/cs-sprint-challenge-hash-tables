d7c9e0e60a2876bee4428f485158d3194b6aa74c
Catherine <catherinesjkim@gmail.com>
2020-09-13 19:16:14 -0700

    Add ex5.py Because: - Implemented a function that given a list of full paths to files and a list of filenames to query, returns all the full paths that match that filename.

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,17 +1,64 @@
-# Your code here
+"""
+# File Finder
 
+Given a list of full paths to files, and a list of filenames to query,
+report all the full paths that match that filename.
 
+Example input:
+
+```python
+paths = [
+    "/usr/local/share/foo.txt",
+    "/usr/bin/ls",
+    "/home/davidlightman/foo.txt",
+    "/bin/su"
+]
+
+queries = [
+    "ls",
+    "foo.txt",
+    "nosuchfile.txt"
+]
+```
+
+Example return value:
+
+```
+[ "/usr/local/share/foo.txt", "/usr/bin/ls", "/home/davidlightman/foo.txt" ]
+```
+
+because that's where the `foo.txt` and `ls` files are. 
+
+The file `"nosuchfile.txt"` is ignored because it's not in the `paths`.
+
+Return list can be in any order.
+
+Limits:
+
+* Up to approximately 1,000,000 paths.
+* Up to approximately 1,000,000 queries.
+
+"""
 
 def finder(files, queries):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    storage, result = {}, []
+    
+    for f in files:
+        file_name = f.split("/")[-1]
+        if file_name in storage:
+            storage[file_name].append(f)
+        else:
+            storage[file_name] = [f]
+    
+    for q in queries:
+        if q in storage:
+            result += storage[q]
 
     return result
 
 
 if __name__ == "__main__":
+    # Input
     files = [
         '/bin/foo',
         '/bin/bar',
@@ -23,3 +70,5 @@ if __name__ == "__main__":
         "baz"
     ]
     print(finder(files, queries))
+
+# Expected Output: ['/bin/foo', '/usr/bin/baz']
